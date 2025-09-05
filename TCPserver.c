5ee94faff3e94bb30ff769675c6abe5672c7bb35
labiadmo920 <labiadmo920@gmail.com>
2025-09-05 18:14:00 +0100

    adding multiple queries handler , using TCP protocol

--- TCPserver.c
+++ TCPserver.c
@@ -1,85 +1,134 @@
-
-
-
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <errno.h> 
 #include <string.h>
+#include <assert.h>
+
+
+#define K_MAX_MSG 4096
 
 static void do_somthing(int connfd); 
+static int32_t read_full(int fd , char *buf , size_t n ); 
+static int32_t write_full(int fd ,const char *buf , size_t n ); 
+static int32_t one_request(int connfd); 
 
 
 int main(int argc,char *argv[])
 {
-    
+    printf("Ready to accept clients \n");
+
     /*step 1 : obtaining the socket using socket()*/
     int fd = socket(
         AF_INET, /* for ipv4 addresses */
         SOCK_STREAM, /* for TCP , SOCK_DGRAM for UDP */
         0
-    ); 
+    );
 
     /* step 2 : configuring the socket options adding REUSEADDR ... */
-    int val = 1 ; 
+    int val = 1 ;
     setsockopt(
         fd, /*the socker */
-        SOL_SOCKET,  
-        SO_REUSEADDR, /* this option allows the program to use the same TCP/IP address without having 
+        SOL_SOCKET,
+        SO_REUSEADDR, /* this option allows the program to use the same TCP/IP address without having
         to wait , the TIME_STOP wich is around 30s to 120s */
-        &val, /* option value in our case its 1 so they are all enabled */ 
+        &val, /* option value in our case its 1 so they are all enabled */
         sizeof(val)
-    ); 
+    );
 
     /* step 3 : binding into an address */
-    struct sockaddr_in addr = {} ; 
-    
-    addr.sin_family = AF_INET;  
-    addr.sin_port = htons(1234); // specefying the port  
-    addr.sin_addr.s_addr = htonl(0); // wildcard IP 0.0.0.0 
-    
+    struct sockaddr_in addr = {} ;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234); // specefying the port
+    addr.sin_addr.s_addr = htonl(0); // wildcard IP 0.0.0.0
+
     int rv = bind(
         fd,
         (const struct sockaddr *)&addr ,
-        sizeof(addr) 
+        sizeof(addr)
     );
 
-    if(rv) /*if it returns 0 */ 
+    if(rv) /*if it returns 0 */
     {
-        perror("Error While Binding the socket with the ipv4:PORT "); 
-        close(rv); 
+        perror("Error While Binding the socket with the ipv4:PORT ");
+        close(rv);
     }
 
     rv = listen(
-        fd,// the socket 
+        fd,// the socket
         SOMAXCONN /* sizeof the queue */
-    ); 
+    );
 
     while(1)
     {
-        //accpting connection
-        struct sockaddr_in client_addr = {} ;  //the client socket 
-        socklen_t addrlen = sizeof(client_addr); 
-        int connfd = accept(fd,(struct sockaddr *)&client_addr, &addrlen);  /* returns the client address */
-        if (connfd < 0 )
-        {
-            perror("Accept error");
-            continue; /* could not establish a connection with the client */
+        struct sockaddr_in client_addr = {} ;
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd <= 0 ) {
+            continue ; /* error */
         }
 
-        printf("Hey Mohamed you will become a cracked dev oneday , and you will be very popular and wealthy !! "); 
-        do_somthing(connfd); 
-        close(connfd); //closing the socket connection 
+        while (1)
+        {
+            int32_t err = one_request(connfd);
+            if (err)
+            {
+                break;
+            }
+        }
+        close(connfd);
     }
 
-    
 
-    return 0 ; 
+
+    return 0 ;
 }
 
+//reading one request and replying on one request
+static int32_t one_request(int connfd)
+{
+    char rbuf[4+K_MAX_MSG];
+    errno = 0 ; 
+    int32_t err = read_full(connfd, rbuf , 4);
+    if (err)
+    {
+        perror(errno == 0 ? "EOF" : "read() error"); 
+        return err; 
+    }
+    uint32_t len =  0 ; /* intialize the msg length */     
+    memcpy(&len , rbuf , 4);  /* memcpy is */
+    if (len > K_MAX_MSG)
+    {
+        perror("msg too long try another "); 
+        return -1; 
+    }
 
-static void do_somthing(int connfd ){
+    err = read_full(connfd, &rbuf[4], len);
+    if(err)
+    {
+        perror("read error()"); 
+        return err ; 
+    } 
+
+    //do somthing 
+    printf("client says:  %.*s\n",  len, rbuf + 4);
+    //reply using the same protocol 
+    /* protocol is [4bytes for len message , msg itself ]*/
+    const char reply[] = "sahbi kon ragel ";
+    char wbuf[4+sizeof(reply)] ; 
+    len = (uint32_t)strlen(reply); 
+
+   
+    memcpy(wbuf ,&len , 4);  /* copy the len of message to the begening 
+    , of memcpy(destination , source , number_of_bytes)*/
+    memcpy(&wbuf[4], reply , len); /* copy the actual message right after the first 4 bytes */
+    
+    return write_full(connfd , wbuf , 4+len);
+} 
+
+static void do_somthing(int connfd )    {
     char rbuf[64] = {}; 
     ssize_t n = read(
         connfd, //peer address 
@@ -106,4 +155,43 @@ static void do_somthing(int connfd ){
     /* we can also use recv instead of read , and send instead of write , but they use flags that we dont want now */
     
 } 
+
+
+static int32_t read_full(int fd , char *buf , size_t n )
+{
+    while(n>0)
+    {
+        ssize_t rv = read(fd,buf,n);
+        if (rv <= 0 )
+        {
+            return -1 ; /* Error or unexpected EOF*/
+        } 
+        assert((size_t)rv<=n); 
+        n-= (size_t)rv ; 
+        buf+= rv ; 
+    }
+    return 0 ; 
+}
+
+static int32_t write_full(int fd ,const char *buf , size_t n )
+{
+    while(n>0)
+    {
+        ssize_t rv = write(fd,buf,n);
+        if(rv <= 0)
+        {
+            return -1; 
+        } 
+        assert((size_t)rv<= n ); /* we use this for the condidtions that should never fail 
+        if its called this means that the code or the OS are malfunctioning 
+        (so this should never fail )
+        */
+        n-= (size_t)rv ; 
+        buf+= rv ; 
+    }
+    return 0 ; 
+}
+
+
+
 
--- client.c
+++ client.c
@@ -1,44 +1,145 @@
-
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h> 
+#include <assert.h>
 
-static void do_somthing(int); 
+#define K_MAX_MSG 4096
 
+static int32_t query(int fd , const char *text); 
+static int32_t read_full(int fd , char *buf , size_t n ); 
+static int32_t write_full(int fd ,const char *buf , size_t n ); 
 
 int main(int argc,char *argv[])
-{   
-
+{
     int fd = socket(AF_INET , SOCK_STREAM, 0);
     if (fd < 0 )
     {
-        close(fd); 
+        perror("socket");
+        return 1;
     }
     
     struct sockaddr_in addr = {}; 
     addr.sin_family = AF_INET ; 
     addr.sin_port = ntohs(1234); 
-    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); //127.0.0.1
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); //127.0.0.1 , is defined by 0x7f000001 localhost basicly 
 
     
     int rv = connect(fd,(const struct sockaddr *)&addr, sizeof(addr)) ; 
 
     if(rv < 0 )
     {
-        perror("connection problem client "); 
+        perror("connection problem client");
+        close(fd); 
+        return -1 ;  // non zero exit means error 
     }
 
-    char msg[] = "hello"; 
-    write(fd,msg,strlen(msg)); 
+    int32_t err = query(fd , "hello1"); 
+    if(err)
+    {
+        goto L_DONE;    
+    }
 
-    char rbuf[64] = {}; 
-    ssize_t n = read(fd,rbuf, sizeof(rbuf)-1);
+    err = query(fd , "hello2"); 
+    if(err)
+    {
+        goto L_DONE ;
+    }
 
-    printf("the server say's %s ", rbuf); 
-    close(fd); 
+    L_DONE : 
+        close(fd); 
+        return 0 ; 
+}
 
+static int32_t query(int fd , const char *text)
+{
+    uint32_t len = (uint32_t) strlen(text); 
+    if (len > K_MAX_MSG)
+    {
+        return -1 ; 
+    }
+
+    //send request
+    char wbuf[4+K_MAX_MSG];
+    memcpy(wbuf , &len , 4 ); //assume little endian  
+    memcpy(&wbuf[4],text , len);
+
+    int32_t err = write_full(fd , wbuf , len +4);
+    if(err)
+    {
+        return err ; 
+    } 
+
+    // receiving the 4byte header
+    char rbuf[4+K_MAX_MSG];
+    uint32_t rlen= 0 ;
+
+    err = read_full(fd , rbuf , 4);
+    if (err)
+    {
+        perror("read length");
+        return err ;
+    }
+
+    // step 2 extract the length
+    memcpy(&rlen, rbuf , 4);
+
+    if(rlen > K_MAX_MSG) {
+        perror("message too long");
+        return -1;
+    }
+
+    // step 3 reading the actual message
+    err = read_full(fd , rbuf +4 , rlen );
+    if (err)
+    {
+        perror("reading the full message error ");
+        return err ;
+    }
+
+    printf("server says %.*s\n" , rlen , rbuf+4);
+    return 0 ;
+}
+
+
+
+
+static int32_t read_full(int fd , char *buf , size_t n )
+{
+    while(n>0)
+    {
+        ssize_t rv = read(fd,buf,n);
+        if (rv <= 0 )
+        {
+            return -1 ; /* Error or unexpected EOF*/
+        } 
+        assert((size_t)rv<=n); 
+        n-= (size_t)rv ; 
+        buf+= rv ; 
+    }
     return 0 ; 
 }
+
+static int32_t write_full(int fd ,const char *buf , size_t n )
+{
+    while(n>0)
+    {
+        ssize_t rv = write(fd,buf,n);
+        if(rv <= 0)
+        {
+            return -1; 
+        } 
+        assert((size_t)rv<= n ); /* we use this for the condidtions that should never fail 
+        if its called this means that the code or the OS are malfunctioning 
+        (so this should never fail )
+        */
+        n-= (size_t)rv ; 
+        buf+= rv ; 
+    }
+    return 0 ; 
+}
+
+
+
